e3d4bc7f55beb927e04df62952e1a3f20015dbd6
Brian Hart <brian.hart@xylogix.net>
2022-06-30 09:58:11 -0600

    Update MainWindow.cs
    * Add XML documentation to the file

--- MyFormsApp_ILMerge/MainWindow.cs
+++ MyFormsApp_ILMerge/MainWindow.cs
@@ -2,11 +2,16 @@
 
 namespace MyFormsApp_ILMerge
 {
+    /// <summary>
+    /// The main window of the application.
+    /// </summary>
     public partial class MainWindow : Form
     {
+        /// <summary>
+        /// Constructs a new instance of <see cref="T:MyFormsApp_ILMerge.MainWindow" /> and
+        /// returns a reference to it.
+        /// </summary>
         public MainWindow()
-        {
-            InitializeComponent();
-        }
+            => InitializeComponent();
     }
 }
