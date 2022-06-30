d9626de26ac2847d7cfe554c61823bb519bcfff5
Brian Hart <brian.hart@xylogix.net>
2022-06-30 09:40:22 -0600

    Create IDocTemplate.cs

---
+++ MyFormsApp_ILMerge.Documents/IDocTemplate.cs
@@ -0,0 +1,17 @@
+﻿namespace MyFormsApp_ILMerge.Documents
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a document template object.
+    /// </summary>
+    public interface IDocTemplate
+    {
+        /// <summary>
+        /// Opens the document having the specified <paramref name="path" />.
+        /// </summary>
+        /// <param name="path">
+        /// (Required.) A <see cref="T:System.String" /> containing the fully-qualified
+        /// pathname of the file you wish to open.
+        /// </param>
+        void OpenDocumentFile(string path);
+    }
+}
