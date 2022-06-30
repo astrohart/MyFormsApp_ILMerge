def5c3b08e6f806d2b4f42985c1504a9d3cc1ca4
Brian Hart <brian.hart@xylogix.net>
2022-06-30 09:40:45 -0600

    Resized the main form a bit

--- MyFormsApp_ILMerge/MainWindow.Designer.cs
+++ MyFormsApp_ILMerge/MainWindow.Designer.cs
@@ -29,10 +29,17 @@ namespace MyFormsApp_ILMerge
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(928, 546);
+            this.Name = "MainWindow";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
