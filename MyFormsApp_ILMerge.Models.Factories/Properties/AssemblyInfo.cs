0e223683a207426dee9f727d5232332321c45572
Brian Hart <brian.hart@xylogix.net>
2022-06-30 10:58:52 -0600

    Create MyFormsApp_ILMerge.Models.Factories.csproj

---
+++ MyFormsApp_ILMerge.Models.Factories/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyFormsApp_ILMerge.Models.Factories")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("MyFormsApp_ILMerge.Models.Factories")]
+[assembly: AssemblyCopyright("Copyright © xyLOGIX, LLC 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3939ae2-f9b1-4fd9-a966-bc2e1e9d95aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
