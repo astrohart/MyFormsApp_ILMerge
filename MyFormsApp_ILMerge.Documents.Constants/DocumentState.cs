0cc0f94a17c35168837db1f2552c41a7e7202ae7
Brian Hart <brian.hart@xylogix.net>
2022-06-30 13:03:11 -0600

    Create DocumentState.cs
    * This is an enumeration, whose values explain what state the document object is in.

---
+++ MyFormsApp_ILMerge.Documents.Constants/DocumentState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFormsApp_ILMerge.Documents.Constants
+{
+    /// <summary>
+    /// Values that explain what state a document object is in.
+    /// </summary>
+    public enum DocumentState
+    {
+        /// <summary>
+        /// The document is in the process of opening a file.
+        /// </summary>
+        Opening,
+
+        /// <summary>
+        /// The document has completed the task of opening a file.
+        /// </summary>
+        Opened,
+
+        /// <summary>
+        /// The document object is not currently carrying out any tasks.
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// The document is in an unknown state.
+        /// </summary>
+        Unknown = -1
+    }
+}
