d744df9c12ff107aeda2dbfc26b4ec5745d4a9a5
Brian Hart <brian.hart@xylogix.net>
2022-06-30 14:16:58 -0600

    Changed the font of the text box in the main form

--- MyFormsApp_ILMerge/MainWindow.Designer.cs
+++ MyFormsApp_ILMerge/MainWindow.Designer.cs
@@ -500,7 +500,7 @@ namespace MyFormsApp_ILMerge
             // fileContentTextBox
             // 
             this.fileContentTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.fileContentTextBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileContentTextBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.fileContentTextBox.Location = new System.Drawing.Point(0, 55);
             this.fileContentTextBox.Multiline = true;
             this.fileContentTextBox.Name = "fileContentTextBox";
