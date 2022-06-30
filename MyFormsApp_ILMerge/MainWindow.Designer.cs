a12a4fe8f603fe5d1158c174706cf38fdb9b2181
Brian Hart <brian.hart@xylogix.net>
2022-06-30 09:49:13 -0600

    Created application and main form icon

--- MyFormsApp_ILMerge/MainWindow.Designer.cs
+++ MyFormsApp_ILMerge/MainWindow.Designer.cs
@@ -468,6 +468,7 @@ namespace MyFormsApp_ILMerge
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.Text = "ILMerge Demo Application";
