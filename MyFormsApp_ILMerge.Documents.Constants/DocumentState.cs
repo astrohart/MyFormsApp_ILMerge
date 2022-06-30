fcae835a68c4fd4eff3232d949ec8e20b9785e2e
Brian Hart <brian.hart@xylogix.net>
2022-06-30 13:03:30 -0600

    Update DocumentState.cs
    * Perform code clean up and apply code style

--- MyFormsApp_ILMerge.Documents.Constants/DocumentState.cs
+++ MyFormsApp_ILMerge.Documents.Constants/DocumentState.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyFormsApp_ILMerge.Documents.Constants
+﻿namespace MyFormsApp_ILMerge.Documents.Constants
 {
     /// <summary>
     /// Values that explain what state a document object is in.
